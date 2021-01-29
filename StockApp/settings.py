c0f83ce3d208e596f9f9f8dfa1bf71631d87a6c4
dreamspeedrun <61959958+dreamspeedrun@users.noreply.github.com>
2021-01-28 21:03:41 -0500

    Update settings.py

--- StockApp/settings.py
+++ StockApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '1_*hcgv3$vaiw^y9)7y5f9f&uv$(v$fpdz-=g+70!qw2_kiu$='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['stockviz-16155.herokuapp.com', '127.0.0.1', '99.237.165.93', '.herokuapp.com']
+ALLOWED_HOSTS = ['https://stockviz-16155.herokuapp.com/', '127.0.0.1', '99.237.165.93', '.herokuapp.com']
 
 
 # Application definition
